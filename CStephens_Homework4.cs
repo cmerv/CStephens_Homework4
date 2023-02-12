76ab6a7c43ae44807e23ed2779736665c5fc0af6
Christopher Stephens <cstep98@outlook.com>
2023-02-12 13:08:59 -0600

    Initial Commit

---
+++ CStephens_Homework4.cs
@@ -0,0 +1,25 @@
+﻿namespace CStephens_Homework4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Select your Method");
+        Console.WriteLine("GreaterThan");
+        Console.WriteLine("TriangleLeft");
+        Console.WriteLine("TriangleRight");
+        string selection = Console.ReadLine().ToLower();
+        //
+        switch (selection){
+        
+        case "GreaterThan":
+        break;
+
+        }
+        
+        GreaterThan();
+    }
+    static void GreaterThan(int number){
+        
+        //
+    }
+}
